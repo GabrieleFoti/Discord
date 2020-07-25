0402ad6010c0d76e827de3ba8329a58248224087
GabrieleFoti <6abri3le@gmail.com>
2020-07-25 18:09:54 +0200

    awd

--- DiscordBot.js
+++ DiscordBot.js
@@ -34,7 +34,9 @@ client.on('guildDelete', (guild) => {
   connection.query(`DELETE FROM \`Guild\` WHERE guild_id = ${guild.id}`, (err) => {})
 
 })
-
+client.on('guildUpdate', (oldGuild, newGuild) => {
+  connection.query(`UPDATE Guild SET name = "${newGuild.name}" WHERE guild_id = ${newGuild.id}`)
+})
 client.on('ready', () => {
   console.log('Ready.')
   client.user.setPresence({activity : {type : 'LISTENING', name : '.help'}, status : 'idle'})
@@ -106,6 +108,11 @@ client.on('message', async message => {
       .setDescription(`✅ unlocked channel ${args}.`)
     message.channel.send(embed)
   }
+  else if(message.content.startsWith(prefix + 'info')){
+    if(!message.author.id === 366345779687981056 && !message.author.id === 359648176791355403) return mesage.reply('you do not have the permission to do that.')
+
+    message.channel.send(`Sto guardando ${client.guilds.cache.array().length} server \nuptime : ${Math.round(client.uptime / 360000)} ore \nutenti : ${client.users.cache.array().length}`)
+  }
   else if(message.content.startsWith(prefix + 'help')){
     message.react('🌑')
     let embedMsg = new Discord.MessageEmbed()
@@ -139,11 +146,13 @@ client.on('message', async message => {
     if(!message.member.permissions.has('KICK_MEMBERS')) return message.reply('you do not have the permission to kick members.')
     args = message.content.split(' ')
     var member = message.mentions.members.first()
-    if(!member){memberSnowflake = args[1]
+    if(!member){
+    memberSnowflake = args[1]
     if(isNaN(memberSnowflake)) return message.reply('you must specify a member to kick')
     member = message.guild.members.cache.get(id = memberSnowflake)
     if(member === undefined) return message.reply('this id does not correspond to any member of the guild')
     }
+    if(message.member.permissions.has('ADMINISTRATOR') && member.permissions.has('ADMINISTRATOR')) return message.reply('you cannot kick another administrator.')
     reason = args.splice(2,).join(' ')
     if(!reason) reason = undefined
     if(member.user.bot) member.kick(reason)
@@ -159,6 +168,7 @@ client.on('message', async message => {
     member = message.guild.members.cache.get(id = memberSnowflake)
     if(member === undefined) return message.reply('this id does not correspond to any member of the guild')
     }
+    if(message.member.permissions.has('ADMINISTRATOR') && member.permissions.has('ADMINISTRATOR')) return message.reply('you cannot ban another administrator.')
     reason = args.splice(2,).join(' ')
     if(!reason) reason = undefined
     if(member.user.bot) member.ban(reason)
@@ -176,8 +186,9 @@ client.on('message', async message => {
     member = message.guild.members.cache.get(id = memberSnowflake)
     if(member === undefined) return message.reply('This id does not correspond to any member of the guild')
     }
+    if(message.member.permissions.has('ADMINISTRATOR') && member.permissions.has('ADMINISTRATOR')) return message.reply('you cannot softban another administrator.')
     reason = args.splice(2,).join(' ')
-    member.send(`You have been banned from ***${message.guild.name}*** for reason: ***${reason}***`).then(() => {member.ban({days : 7, reason : reason}); message.guild.members.unban(member)})
+    member.send(`You have been kicked from ***${message.guild.name}*** for reason: ***${reason}***`).then(() => {member.ban({days : 7, reason : reason}); message.guild.members.unban(member)})
 
   }
   else if(message.content.startsWith(prefix + 'unban')){
@@ -281,9 +292,11 @@ client.on('message', async message => {
     if(channel === undefined) return message.reply('you must set a channel for the welcome message.')
     if(toggle === 'on') connection.query(`UPDATE Guild SET welcome_msg = 1, welcome_channel_id = ${channel.id} WHERE guild_id = ${message.guild.id}`, (err) => {
       if(err) message.reply('there was an error setting the channel, please try again.')
+      else message.reply('welcome message turned on.')
       })
       else connection.query(`UPDATE Guild SET welcome_msg = 0, welcome_channel_id = NULL WHERE guild_id = ${message.guild.id}`, (err) => {
         if(err) message.reply('there was an error switching off the command, please try again.')
+        else message.reply('welcome message turned off.')
       })
   }else if(message.content.startsWith(prefix + 'autorole')){
     if(!message.member.permissions.has('MANAGE_GUILD')) return message.reply('you do not have the permission to perform this action.')
